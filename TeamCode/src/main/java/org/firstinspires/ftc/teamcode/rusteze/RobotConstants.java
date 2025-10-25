83c45486540a35f60d56c2c37ebe937c88d6e106
jonas-ryan <j.e.ryan2000@gmail.com>
2025-10-24 23:42:25 -0700

    Add configurable FTC Dashboard constant class

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/rusteze/RobotConstants.java
@@ -0,0 +1,12 @@
+package org.firstinspires.ftc.teamcode.rusteze;
+
+import com.acmerobotics.dashboard.config.Config;
+
+@Config
+public class RobotConstants {
+    public static int TARGET_RPM = 0;
+
+    public int getTargetRpm() {
+        return TARGET_RPM;
+    }
+}
