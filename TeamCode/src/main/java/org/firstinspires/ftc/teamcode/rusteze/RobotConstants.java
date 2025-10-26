ab8f9fa7237e238db3136e95a95446ae58409cbf
jonas-ryan <j.e.ryan2000@gmail.com>
2025-10-25 19:51:10 -0700

    Add kV and kP values to ftc dashboard configurable variables

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/rusteze/RobotConstants.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/rusteze/RobotConstants.java
@@ -5,8 +5,16 @@ import com.acmerobotics.dashboard.config.Config;
 @Config
 public class RobotConstants {
     public static int TARGET_RPM = 0;
+    public static double K_V = 0.0002;
+    public static double K_P = 0.001;
 
     public int getTargetRpm() {
         return TARGET_RPM;
+    }
+    public double getkV() {
+        return K_V;
+    }
+    public double getkP() {
+        return K_P;
     }
 }
