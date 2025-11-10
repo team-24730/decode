df1aaece2fcdbd3e582a503df9738778e312cf19
jonas-ryan <j.e.ryan2000@gmail.com>
2025-11-09 17:15:01 -0800

    Create transfer class

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/rusteze/Transfer.java
@@ -0,0 +1,20 @@
+package org.firstinspires.ftc.teamcode.rusteze;
+
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorEx;
+import com.qualcomm.robotcore.hardware.DcMotorSimple;
+import com.qualcomm.robotcore.hardware.HardwareMap;
+
+public class Transfer {
+    public DcMotorEx transfer;
+
+    public Transfer(HardwareMap hwMap) {
+        transfer = hwMap.get(DcMotorEx.class, "transfer");
+        transfer.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        transfer.setDirection(DcMotorSimple.Direction.REVERSE);
+    }
+
+    public void setPower(double power) {
+        transfer.setPower(power);
+    }
+}
