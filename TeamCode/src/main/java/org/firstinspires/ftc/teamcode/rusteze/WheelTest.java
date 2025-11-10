d9058b4f62a748ee357e8caef0ab3c812228b08e
jonas-ryan <j.e.ryan2000@gmail.com>
2025-11-09 17:16:11 -0800

    Create wheel test where each individual wheel can be tested at different speeds

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/rusteze/WheelTest.java
@@ -0,0 +1,58 @@
+package org.firstinspires.ftc.teamcode.rusteze;
+
+import com.qualcomm.robotcore.eventloop.opmode.Disabled;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorEx;
+import com.qualcomm.robotcore.hardware.DcMotorSimple;
+import com.qualcomm.robotcore.util.ElapsedTime;
+
+
+@TeleOp(name="WheelTest")
+public class WheelTest extends LinearOpMode {
+    private ElapsedTime runtime = new ElapsedTime();
+
+    private DcMotorEx frontLeft;
+    private DcMotorEx backLeft;
+    private DcMotorEx backRight;
+    private DcMotorEx frontRight;
+
+    @Override
+    public void runOpMode() {
+
+        frontLeft  = hardwareMap.get(DcMotorEx.class, "frontLeft" );
+        backLeft   = hardwareMap.get(DcMotorEx.class, "backLeft"  );
+        backRight  = hardwareMap.get(DcMotorEx.class, "backRight" );
+        frontRight = hardwareMap.get(DcMotorEx.class, "frontRight");
+
+        frontLeft .setDirection(DcMotorSimple.Direction.REVERSE);
+        backLeft  .setDirection(DcMotorSimple.Direction.REVERSE);
+        backRight .setDirection(DcMotorSimple.Direction.FORWARD);
+        frontRight.setDirection(DcMotorSimple.Direction.FORWARD);
+
+        frontLeft .setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        backLeft  .setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        backRight .setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        frontRight.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+
+        waitForStart();
+        runtime.reset();
+        while(opModeIsActive()) {
+
+            if (gamepad1.x) {
+                frontLeft.setPower(-gamepad1.left_stick_y);
+            }
+            if (gamepad1.a) {
+                backLeft.setPower(-gamepad1.left_stick_y);
+            }
+            if (gamepad1.b) {
+                backRight.setPower(-gamepad1.left_stick_y);
+            }
+            if (gamepad1.y) {
+                frontRight.setPower(-gamepad1.left_stick_y);
+            }
+
+        }
+    }
+}
