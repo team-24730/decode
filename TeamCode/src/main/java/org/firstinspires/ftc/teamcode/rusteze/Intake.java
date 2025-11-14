09bdc604321b4edb2402019dac39608087a069b5
jonas-ryan <j.e.ryan2000@gmail.com>
2025-11-13 18:53:37 -0800

    Make enable and disable functions for intake class

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/rusteze/Intake.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/rusteze/Intake.java
@@ -6,6 +6,7 @@ import com.qualcomm.robotcore.hardware.HardwareMap;
 
 public class Intake {
     DcMotorEx intake;
+    public boolean isEnabled;
 
     public Intake(HardwareMap hwMap) {
         intake = hwMap.get(DcMotorEx.class, "intake");
@@ -14,5 +15,15 @@ public class Intake {
 
     public void setPower(double power) {
         intake.setPower(power);
+    }
+
+    public void disable() {
+        setPower(0);
+        isEnabled = false;
+    }
+
+    public void enable() {
+        setPower(1);
+        isEnabled = true;
     }
 }
