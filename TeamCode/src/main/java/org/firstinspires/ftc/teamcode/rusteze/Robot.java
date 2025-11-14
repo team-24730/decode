c28de92a01da32cea7749775a86223ebed17b479
jonas-ryan <j.e.ryan2000@gmail.com>
2025-11-13 18:55:04 -0800

    Create a .update() function and add a system for reversing the intake for some time after intaking

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/rusteze/Robot.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/rusteze/Robot.java
@@ -8,10 +8,38 @@ public class Robot {
     public Transfer transfer;
     public Outtake outtake;
 
+    public boolean intakeRunning = false;
+
+    long lastIntakeTime = 0;
+    boolean turnShooterOffFlag = false;
+
     public Robot(HardwareMap hwMap) {
         drivetrain = new Drivetrain(hwMap);
         intake = new Intake(hwMap);
         transfer = new Transfer(hwMap);
         outtake = new Outtake(hwMap);
+    }
+
+    public void update() {
+        if (intakeRunning) {
+            intake.setPower(1);
+            transfer.setPower(0.3);
+            lastIntakeTime = System.currentTimeMillis();
+        } else if (System.currentTimeMillis() - lastIntakeTime < 500) {
+            intake.disable();
+            transfer.setPower(-0.3);
+            outtake.setPower(-0.35);
+            turnShooterOffFlag = true;
+        } else {
+            intake.disable();
+            transfer.disable();
+            if (turnShooterOffFlag) {
+                outtake.setFlywheelTarget(0);
+                outtake.setPower(0);
+                turnShooterOffFlag = false;
+            }
+        }
+
+
     }
 }
