61252a74a4c779038a38588990b13d9350941971
jonas-ryan <j.e.ryan2000@gmail.com>
2026-01-04 11:09:01 -0800

    Add a lift class to contain the linear servos for endgame

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/rusteze/Lift.java
@@ -0,0 +1,32 @@
+package org.firstinspires.ftc.teamcode.rusteze;
+
+import com.qualcomm.robotcore.hardware.HardwareMap;
+import com.qualcomm.robotcore.hardware.Servo;
+
+public class Lift {
+    public Servo leftLift;
+    public Servo rightLift;
+    public boolean extended;
+
+    public Lift(HardwareMap hwMap) {
+        leftLift = hwMap.get(Servo.class, "leftLift");
+        rightLift = hwMap.get(Servo.class, "rightLift");
+        extended = false;
+    }
+
+    public void retract() {
+        if (extended) {
+            leftLift.setPosition(0.15);
+            rightLift.setPosition(0.15);
+            extended = false;
+        }
+    }
+
+    public void extend() {
+        if (!extended) {
+            leftLift.setPosition(0.78);
+            rightLift.setPosition(0.78);
+            extended = true;
+        }
+    }
+}
