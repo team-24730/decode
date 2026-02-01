ac46d82221349556f90e4ec35fab1af795031306
jonas-ryan <j.e.ryan2000@gmail.com>
2026-01-31 23:39:13 -0800

    Create a new robot class for V2 of the robot (copied from the old robot class) and edit it for V2 of the robot

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/rusteze/RobotV2.java
@@ -0,0 +1,39 @@
+package org.firstinspires.ftc.teamcode.rusteze;
+
+import com.qualcomm.robotcore.hardware.HardwareMap;
+
+public class RobotV2 {
+    public Drivetrain drivetrain;
+    public Intake intake;
+    public Outtake outtake;
+    public Lift lift;
+
+    public enum State {
+        IDLE,
+        INTAKE,
+        SHOOTING,
+        LIFT
+    }
+    State robotState = State.IDLE;
+
+    public RobotV2(HardwareMap hwMap) {
+        drivetrain = new Drivetrain(hwMap);
+        intake = new Intake(hwMap);
+        outtake = new Outtake(hwMap);
+        lift = new Lift(hwMap);
+    }
+
+    public State getState() {
+        return robotState;
+    }
+
+    public void setState(State state) {
+        this.robotState = state;
+    }
+
+    public void update() {
+
+        intake.update();
+        outtake.update();
+    }
+}
