1dbf74fe40acb7a96c100c8528c0b603b1f688bc
jonas-ryan <j.e.ryan2000@gmail.com>
2026-02-06 16:59:45 -0800

    Change clutch logic

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/rusteze/Intake.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/rusteze/Intake.java
@@ -4,18 +4,17 @@ import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.DcMotorEx;
 import com.qualcomm.robotcore.hardware.HardwareMap;
 import com.qualcomm.robotcore.hardware.Servo;
-import com.qualcomm.robotcore.util.ElapsedTime;
 
 public class Intake {
     DcMotorEx intake;
     Servo clutch;
-    public boolean isEnabled;
+    public boolean clutchEnabled;
     long timestamp;
 
 
     public enum State {
         INTAKE,
-        REENGAGING,
+        REVERSE,
         TRANSFER,
         IDLE
     }
@@ -33,9 +32,19 @@ public class Intake {
 
     public void enable() {
         state = State.INTAKE;
+        if (clutchEnabled) {
+            intake.setPower(0.2);
+        } else {
+            intake.setPower(1);
+        }
+        timestamp = System.currentTimeMillis();
         disengageClutch();
-        intake.setPower(0);
-        timestamp = System.currentTimeMillis(); // delay intaking
+    }
+
+    public void reverse() {
+        state = State.REVERSE;
+        disengageClutch();
+        intake.setPower(-1);
     }
 
     public void enableTransfer() {
@@ -45,24 +54,16 @@ public class Intake {
     }
 
     public void disable() {
-        intake.setPower(0);
         state = State.IDLE;
+        intake.setPower(0);
     }
 
-    public void engageClutch() { clutch.setPosition(0.6); }
+    public void engageClutch() { clutch.setPosition(0.6); clutchEnabled = true; }
 
-    public void disengageClutch () { clutch.setPosition(0.8); }
+    public void disengageClutch () { clutch.setPosition(0.8); clutchEnabled = false; }
 
     public void update() {
-        /*if (state == State.REENGAGING && System.currentTimeMillis() - timestamp > 250) { // reengage after a delay
-            engageClutch();
-        }
-        if (state == State.REENGAGING && System.currentTimeMillis() - timestamp > 1000) { // allow one second and a half for the clutch to reengage
-            intake.setPower(0);
-            state = State.IDLE;
-        } */
-
-        if (state == State.INTAKE && System.currentTimeMillis() - timestamp > 150) { // allow some time for disengagement before intaking
+        if (state == State.INTAKE && System.currentTimeMillis() - timestamp > 300) { // allow some time for disengagement before intaking
             intake.setPower(1);
         }
     }
