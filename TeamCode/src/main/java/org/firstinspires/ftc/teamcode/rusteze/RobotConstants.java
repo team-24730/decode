f2a9f49053f67f1f5e17617a7e96899ff533c92f
jonas-ryan <j.e.ryan2000@gmail.com>
2026-02-06 16:56:56 -0800

    Add turret initialization boolean and update outtake PID values

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/rusteze/RobotConstants.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/rusteze/RobotConstants.java
@@ -5,14 +5,15 @@ import com.acmerobotics.dashboard.config.Config;
 @Config
 public class RobotConstants {
     public static int TARGET_RPM = 3690;
-    public static double K_V = 0.0002;
-    public static double K_P = 0.001;
+    public static double K_V = 0.0002; // 0.0002
+    public static double K_P = 0.005; // 0.001
     public static double TARGET_HOOD = 0.4 ; // 0.25 - 0.97
     public enum Color {
         RED,
         BLUE
     }
     public static Color teamColor = Color.RED;
+    public static boolean turretInitialized = false;
 
     public int getTargetRpm() {
         return TARGET_RPM;
