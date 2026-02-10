6810cfdab8e960c9dec4b2cbdc3178756c0fac47
jonas-ryan <j.e.ryan2000@gmail.com>
2026-02-09 22:05:32 -0800

    Adjust auto slightly for better shooting consistency

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/rusteze/autos/V2RobotRedClose.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/rusteze/autos/V2RobotRedClose.java
@@ -19,7 +19,7 @@ import com.qualcomm.robotcore.util.ElapsedTime;
 
 import org.firstinspires.ftc.teamcode.MecanumDrive;
 import org.firstinspires.ftc.teamcode.rusteze.RobotConstants;
-import org.firstinspires.ftc.teamcode.rusteze.RobotV2;
+import org.firstinspires.ftc.teamcode.rusteze.subsystems.RobotV2;
 
 @Autonomous(name="Red Close Main", group="main")
 public class V2RobotRedClose extends LinearOpMode {
@@ -62,7 +62,7 @@ public class V2RobotRedClose extends LinearOpMode {
                 if (!initialized) {
                     robot.outtake.useControlSystem = true;
                     robot.outtake.setTarget(3300);
-                    robot.outtake.setHood(0.4);
+                    robot.outtake.setHood(0.6);
                     initialized = true;
                     elapsedTime.reset();
                 }
@@ -224,7 +224,7 @@ public class V2RobotRedClose extends LinearOpMode {
                         return 30;
                     }
                 })
-                .afterTime(1.5, new IntakeOff())
+                .afterTime(1.1, new IntakeOff())
                 .afterTime(0.7, new SpinUpFlywheelMedium())
                 .afterTime(1.5, new Shoot())
                 .strafeToLinearHeading(new Vector2d(-26, 8), Math.toRadians(135))
