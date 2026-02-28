da46935226e06fae2fb281b2bb84ec69431dca59
jonas-ryan <j.e.ryan2000@gmail.com>
2026-02-27 18:23:47 -0800

    Adjust tick offset of odometry pods

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/PinpointLocalizer.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/PinpointLocalizer.java
@@ -16,8 +16,8 @@ import java.util.Objects;
 @Config
 public final class PinpointLocalizer implements Localizer {
     public static class Params {
-        public double parYTicks = -2242.29512650058; // y position of the parallel encoder (in tick units)
-        public double perpXTicks = -2677.9434042283433; // x position of the perpendicular encoder (in tick units)
+        public double parYTicks = -2081.190987945921; // y position of the parallel encoder (in tick units)
+        public double perpXTicks = -1809.5708450074674; // x position of the perpendicular encoder (in tick units)
     }
 
     public static Params PARAMS = new Params();
