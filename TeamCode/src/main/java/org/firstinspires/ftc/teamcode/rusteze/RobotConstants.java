8a195a69f6e756b2e1cac9898f28ca98dab8254b
jonas-ryan <j.e.ryan2000@gmail.com>
2026-02-27 18:25:57 -0800

    Adjust feedforward and feedback constants for outtake RPM control

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/rusteze/RobotConstants.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/rusteze/RobotConstants.java
@@ -4,9 +4,9 @@ import com.acmerobotics.dashboard.config.Config;
 
 @Config
 public class RobotConstants {
-    public static int TARGET_RPM = 3690;
-    public static double K_V = 0.0002; // 0.0002
-    public static double K_P = 0.005; // 0.001
+    public static int TARGET_RPM = 3000;
+    public static double K_V = 0.0004; // //0.0002    Oldest: 0.0002
+    public static double K_P = 0.1; // 0.005        Oldest: 0.001
     public static double TARGET_HOOD = 0.4 ; // 0.25 - 0.97
     public enum Color {
         RED,
