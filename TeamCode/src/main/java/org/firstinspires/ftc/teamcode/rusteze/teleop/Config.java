08a18a7c30ed19263a946cff9722f5d96261e21f
jonas-ryan <j.e.ryan2000@gmail.com>
2026-02-27 18:18:49 -0800

    Add turret control to the config

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/rusteze/teleop/Config.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/rusteze/teleop/Config.java
@@ -2,21 +2,27 @@ package org.firstinspires.ftc.teamcode.rusteze.teleop;
 
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorEx;
 import com.qualcomm.robotcore.util.ElapsedTime;
 
 import org.firstinspires.ftc.teamcode.rusteze.RobotConstants;
+import org.firstinspires.ftc.teamcode.rusteze.subsystems.Turret;
 
 
 @TeleOp(name="Config")
 public class Config extends LinearOpMode {
     private ElapsedTime runtime = new ElapsedTime();
     private RobotConstants constants = new RobotConstants();
+    DcMotorEx turret;
+    DcMotorEx intake;
 
 
     @Override
     public void runOpMode() {
 
-
+        turret = hardwareMap.get(DcMotorEx.class, "turret");
+        turret.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
 
         waitForStart();
         runtime.reset();
@@ -30,9 +36,20 @@ public class Config extends LinearOpMode {
                 }
             }
 
+            if (Math.abs(gamepad1.left_stick_x) > 0.05) {
+                turret.setPower(gamepad1.left_stick_x * 0.3);
+            } else {
+                turret.setPower(0);
+            }
+
+            if (gamepad1.y) {
+                RobotConstants.turretInitialized = false;
+            }
 
             telemetry.addLine("Press a to change color");
             telemetry.addData("Current Color", constants.teamColor);
+            telemetry.addLine("Use left joystick to move turret");
+            telemetry.addData("Press y to deinitialize turret", RobotConstants.turretInitialized);
             telemetry.update();
 
 
