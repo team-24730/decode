5736a5df78260e75ae81c0d80455665fb9130166
jonas-ryan <j.e.ryan2000@gmail.com>
2026-04-25 14:21:40 -0700

    Change kP to 0.5 for the outtake PID

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/rusteze/RobotConstants.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/rusteze/RobotConstants.java
@@ -6,7 +6,7 @@ import com.acmerobotics.dashboard.config.Config;
 public class RobotConstants {
     public static int TARGET_RPM = 3000;
     public static double K_V = 0.0004; // //0.0002    Oldest: 0.0002
-    public static double K_P = 0.1; // 0.005        Oldest: 0.001
+    public static double K_P = 0.5; // 0.005        Oldest: 0.001
     public static double TARGET_HOOD = 0.4 ; // 0.25 - 0.97
     public enum Color {
         RED,
